6e4898156610685e9785de6aa50741ac1d938a4c
Irtiqa <mirirtiqa66@gmail.com>
2025-05-23 22:50:43 +0530

    bug fix

--- config/gcs.js
+++ config/gcs.js
@@ -1,15 +1,22 @@
 import { Storage } from '@google-cloud/storage';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
+const credentials = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
-const storage = new Storage({
-  keyFilename: path.join(__dirname, '../gcsServiceAccount.json'),
-});
+const storage = new Storage({ credentials });
 
 const bucket = storage.bucket('spotdraft_bucket');
 
